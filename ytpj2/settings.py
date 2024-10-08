e9fc3f54327f42c9c79ba8d7bed9524a66716ab2
user <user@usernoMacBook-Air.local>
2024-10-08 14:31:02 +0900

    23ch ok

--- ytpj2/settings.py
+++ ytpj2/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +136,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    # "site_title": "MovieX",
+    "site_header": "Youtube Studio",
+    "site_brand": "Think | Create | Inspire",
+    "copyright": "youtube.com",
+    # "site_logo": "images/logo.jpg",
+    # "order_with_respect_to": ["blog"]
+}
